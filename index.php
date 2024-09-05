e2684468b1a5a9f9a2690c1845e1646fb99415b4
Francescosommellaa <franc.sommella@gmail.com>
2024-09-05 19:25:02 +0200

    Create Models

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+
+// importo la classe.
+require_once __DIR__ . '/Model/Category.php';
+require_once __DIR__ . '/data/db.php';
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.3/css/bootstrap-grid.css' integrity='sha512-hhSu9overYjKfSjPCtJW3688VHkfBh+W1pR5Mysll91bOJwGjYntytGTtVXb2aisFOaYXXDrO38NKXDRPJWu7A==' crossorigin='anonymous' />
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Negozio di prodotti per animali</title>
+</head>
+
+<body>
